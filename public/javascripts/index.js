700399d5b101afac7b4b29a479e7fda75a98c81c
Travis CI <build-travis@6700.com.ar>
2017-09-24 00:13:07 -0300

    Basic slide system

---
+++ public/javascripts/index.js
@@ -0,0 +1,33 @@
+(function(){
+  let currentSlideNumber = 0;
+
+  const getSlides = () => document.querySelectorAll(`.slide`)
+  const getSlide = (slide) => document.querySelector(`.slide[data-pos=${slide}]`)
+  const getNotSlide = (slide) => document.querySelectorAll(`.slide:not([data-pos='${slide}'])`)
+  const getCurrentSlide = () => getSlide(currentSlideNumber)
+  const getNotCurrentSlide = () => getNotSlide(currentSlideNumber)
+  const maxSlide = getSlides().length - 1
+  const minSlide = 0
+  const nextSlide = () => { maxSlide < currentSlideNumber + 1 || currentSlideNumber ++ ; updateSlide() }
+  const prevSlide = () => { minSlide > currentSlideNumber - 1 || currentSlideNumber -- ; updateSlide() }
+
+  const updateSlide = () => {
+    getSlides().forEach((slide) => {
+      slide.style.left = `${(slide.dataset.pos - currentSlideNumber) * slide.offsetWidth}px`
+    })
+  }
+
+  document.addEventListener('keyup', (e) => {
+    switch (e.keyCode) {
+      case 39:
+        nextSlide()
+        break;
+      case 37:
+        prevSlide()
+        break;
+      default:
+    }
+  })
+
+  updateSlide()
+})()
