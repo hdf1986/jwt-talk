8298ce1f430163c9c173c1573275240fd9cc6acb
Travis CI <build-travis@6700.com.ar>
2017-09-24 01:43:47 -0300

    Fixed animations

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,11 @@
 (function(){
   const getPathUrl = () => document.location.hash.substr(1);
   const setPathUrl = (hash) => document.location.hash = hash;
+  const slideWidth = document.querySelector('.slides').offsetWidth;
   let currentSlideNumber = getPathUrl() || 0;
 
   const getSlides = () => document.querySelectorAll(`.slide`)
-  const getSlide = (slide) => document.querySelector(`.slide[data-pos=${slide}]`)
+  const getSlide = (slide) => document.querySelector(`.slide[data-pos='${slide}']`)
   const getNotSlide = (slide) => document.querySelectorAll(`.slide:not([data-pos='${slide}'])`)
   const getCurrentSlide = () => getSlide(currentSlideNumber)
   const getNotCurrentSlide = () => getNotSlide(currentSlideNumber)
@@ -16,7 +17,10 @@
   const updateSlide = () => {
     setPathUrl(currentSlideNumber);
     getSlides().forEach((slide) => {
-      slide.style.left = `${(slide.dataset.pos - currentSlideNumber) * slide.offsetWidth}px`
+      slide.style.left = `${(slide.dataset.pos - currentSlideNumber) * slideWidth}px`
+    })
+    getCurrentSlide().querySelectorAll('[data-animate]').forEach((ele) => {
+      ele.classList.add(ele.dataset.animate, 'animated')
     })
   }
 
